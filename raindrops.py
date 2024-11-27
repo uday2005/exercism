423c383df5443d21d4367cc1cefe02b7f7418f9d
Uday kumar <24b2729@iitb.ac.in>
2024-11-27 13:40:11 +0530

    checking some cases by uses of print

--- raindrops.py
+++ raindrops.py
@@ -2,10 +2,21 @@ def convert(number):
     why = []
     if number %3 ==  0 :
         why.append("Pling")
+        print(why)
     elif number % 5 == 0:
         why.append("Plang")
+        print(why)
     elif number % 7 == 0:
         why.append("Plong")
+        print(why)
     else:
         return str(number)
-    return why[0]
+    return why[:-1]
+
+# but the problem here is happening when the number is divisble by both 3 and 5 
+# it should brong plingplang but it is not happening  
+convert(3)
+convert(5)
+convert(7)
+convert(43)
+convert(105)
