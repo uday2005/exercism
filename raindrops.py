440a002823212d40dd078681c8aa70b069a1f45f
Uday kumar <24b2729@iitb.ac.in>
2024-11-27 14:47:41 +0530

    some more comments adn change the elif to if

--- raindrops.py
+++ raindrops.py
@@ -1,22 +1,26 @@
 def convert(number):
-    why = []
+    why =  []
     if number %3 ==  0 :
         why.append("Pling")
-        print(why)
-    elif number % 5 == 0:
+    if number % 5 == 0:
         why.append("Plang")
-        print(why)
-    elif number % 7 == 0:
+    if number % 7 == 0:
         why.append("Plong")
-        print(why)
+    why1 = "".join(why)
+    return why1 if why1 else str(number)
+
+    if why:
+        return "".join(why)
     else:
         return str(number)
-    return why[:-1]
+
 
 # but the problem here is happening when the number is divisble by both 3 and 5 
 # it should brong plingplang but it is not happening  
-convert(3)
-convert(5)
-convert(7)
-convert(43)
-convert(105)
+
+# so to do that we can use if caondtion all the way as elif only gets activated if the if statement is false
+
+# the next problem will be when we will use else right before it check onlt the case in which number %7 ==0
+
+# we use the .join function for dictionary
+
